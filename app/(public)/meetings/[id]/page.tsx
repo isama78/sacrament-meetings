d3fb337ae205ebee57e12db32396981a2c4ff648
isama78 <isa21005@byui.edu>
2026-07-27 06:46:49 -0300

    debugging

--- app/(public)/meetings/[id]/page.tsx
+++ app/(public)/meetings/[id]/page.tsx
@@ -1,4 +1,5 @@
 import MeetingDetail from '@/components/MeetingDetail';
+import { getMeetingById } from '@/lib/meetings-db';
 import { SacramentMeeting } from '@/lib/types';
 import { notFound } from 'next/navigation';
 
@@ -6,20 +7,20 @@ interface MeetingPageProps {
   params: Promise<{ id: string }>;
 }
 
-async function fetchMeeting(id: string): Promise<SacramentMeeting | null> {
-  // Simulate a delay to show loading loading state
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/api/meetings/${id}`);
-  if (res.status === 404 || res.status === 400) {
-    return null;
-  }
-  if (!res.ok) throw new Error('Error fetching meeting details');
-  return res.json();
-}
+// async function fetchMeeting(id: string): Promise<SacramentMeeting | null> {
+//   // Simulate a delay to show loading loading state
+//   await new Promise((resolve) => setTimeout(resolve, 1000));
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/api/meetings/${id}`);
+//   if (res.status === 404 || res.status === 400) {
+//     return null;
+//   }
+//   if (!res.ok) throw new Error('Error fetching meeting details');
+//   return res.json();
+// }
 
 export default async function MeetingDetailPage({ params }: MeetingPageProps) {
   const { id } = await params;
-  const meeting = await fetchMeeting(id);
+  const meeting = await getMeetingById(Number(id));
 
   if (!meeting) {
     notFound();
